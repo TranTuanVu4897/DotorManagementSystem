38a4bf3c18c0542e5b7d8c2624a800e2abf0c5e1
TranTuanVu4897 <46296441+TranTuanVu4897@users.noreply.github.com>
2020-10-01 20:18:08 +0700

    downgrade to 8.2

--- src/controller/Control.java
+++ src/controller/Control.java
@@ -6,11 +6,9 @@
 package controller;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
-import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import model.Doctor;
